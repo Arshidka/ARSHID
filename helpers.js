078e76a0bd796661f339541cd0c9648cf5297de5
Arshidka <89933485+Arshidka@users.noreply.github.com>
2021-09-22 23:00:56 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *Arshidser*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *Arshidser*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *Arshidser*:  ```" + msg + "```"
 }
 
 
